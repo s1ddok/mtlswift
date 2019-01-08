9b4f84cb1eb9f373013968a89f16c7c49a046274
s1ddok <siddok@gmail.com>
2019-01-08 15:48:50 +0300

    simple source builder class

---
+++ Sources/mtlswift/SourceStringBuilder.swift
@@ -0,0 +1,30 @@
+//
+//  SourceStringBuilder.swift
+//  mtlswift
+//
+//  Created by Andrey Volodin on 08/01/2019.
+//
+
+public class SourceStringBuilder {
+    public var identationLevel = "  "
+
+    fileprivate(set) public var currentIdentation = ""
+    
+    public func pushLevel() {
+        currentIdentation += self.identationLevel
+    }
+    
+    public func popLevel() {
+        currentIdentation.removeLast(self.identationLevel.count)
+    }
+    
+    public var result: String = ""
+    
+    public func begin() {
+        result = "\n"
+    }
+    
+    public func add(line: String) {
+        result += self.currentIdentation + line + "\n"
+    }
+}

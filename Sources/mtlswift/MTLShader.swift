8132e893639b9dfd9be967c19d0eecf10eac5047
s1ddok <siddok@gmail.com>
2019-01-08 15:49:19 +0300

    prototype implementation of kernel generation

--- Sources/mtlswift/MTLShader.swift
+++ Sources/mtlswift/MTLShader.swift
@@ -37,4 +37,75 @@ public struct MTLShader {
         \(self.parameters.map { $0.description })
 """
     }
+    
+    public func generateSwiftSource(in builder: SourceStringBuilder) {
+        guard self.kind == .kernel else {
+            print("At this point only kernel generation is implemented")
+            return
+        }
+        
+        builder.add(line:"public class \(name)Encoder {")
+        builder.pushLevel()
+        builder.add(line: "fileprivate let pipelineState: MTLComputePipelineState")
+        
+        builder.add(line: "")
+        
+        for parameter in self.parameters {
+            let swiftType: String
+            
+            switch parameter.kind {
+            case .buffer: swiftType = "MTLBuffer"
+            case .sampler: swiftType = "MTLSamplerState"
+            case .texture: swiftType = "MTLTexture"
+            default: continue
+            }
+            builder.add(line: "public var \(parameter.name): \(swiftType)? = nil")
+            
+            switch parameter.kind {
+            case .buffer:
+                builder.add(line: "public var \(parameter.name)Offset: Int = 0")
+            default: continue
+            }
+            
+        }
+        
+        builder.add(line: "")
+        
+        builder.add(line: "public init(library: MTLLibrary) throws {")
+        builder.pushLevel()
+        
+        builder.add(line: "self.pipelineState = try library.computePipelineState(function: \"\(self.name)\")")
+        
+        builder.popLevel()
+        builder.add(line: "}")
+        
+        builder.add(line: "")
+        
+        builder.add(line: "func encode(using encoder: MTLComputeCommandEncoder) {")
+        builder.pushLevel()
+        
+        for parameter in self.parameters {
+            switch parameter.kind {
+            case .buffer:
+                builder.add(line: "encoder.setBuffer(self.\(parameter.name), offset: self.\(parameter.name)Offset, index: \(parameter.index!))")
+            case .sampler:
+                builder.add(line: "encoder.setSamplerState(self.\(parameter.name), index: \(parameter.index!))")
+            case .texture:
+                builder.add(line: "encoder.setTexture(self.\(parameter.name), index: \(parameter.index!))")
+            default: continue
+            }
+        }
+        
+        builder.add(line: "")
+        
+        builder.add(line: "encoder.setComputePipelineState(self.pipelineState)")
+        
+        builder.popLevel()
+        builder.add(line: "}")
+        
+        builder.add(line: "")
+        
+        builder.popLevel()
+        builder.add(line: "}")
+    }
 }

4114f813138ab74d7321e371718dbfe360054128
s1ddok <siddok@gmail.com>
2019-01-10 20:50:57 +0300

    parameterize generation with declarations

--- Sources/mtlswift/MTLShader.swift
+++ Sources/mtlswift/MTLShader.swift
@@ -35,6 +35,7 @@ public struct MTLShader {
     public var name: String
     public var kind: Kind
     public var parameters: [Parameter]
+    public var customDeclarations: [CustomDeclaration]
     
     public var description: String {
         return """
@@ -52,33 +53,89 @@ public struct MTLShader {
             return
         }
         
-        builder.add(line:"public class \(name.capitalizingFirstLetter)Encoder {")
+        var accessLevel: CustomDeclaration.AccessLevel = .internal
+        if let accessLevelDeclaration = self.customDeclarations.first(where: { d -> Bool in
+            if case .accessLevel(_) = d {
+                return true
+            } else {
+                return false
+            }
+        }) {
+            if case .accessLevel(let level) = accessLevelDeclaration {
+                accessLevel = level
+            }
+        }
+        
+        if let nameDeclaration = self.customDeclarations.first(where: { d -> Bool in
+            if case .swiftName(_) = d {
+                return true
+            } else {
+                return false
+            }
+        }) {
+            if case .swiftName(let name) = nameDeclaration {
+                builder.add(line:"\(accessLevel.rawValue) class \(name) {")
+            }
+        } else {
+            builder.add(line:"\(accessLevel.rawValue) class \(name.capitalizingFirstLetter)Encoder {")
+        }
         builder.pushLevel()
-        builder.add(line: "fileprivate let pipelineState: MTLComputePipelineState")
+        builder.add(line: "\(accessLevel.rawValue) let pipelineState: MTLComputePipelineState")
         
         builder.add(line: "")
         
-        for parameter in self.parameters {
-            let swiftType: String
-            
-            switch parameter.kind {
-            case .buffer: swiftType = "MTLBuffer"
-            case .sampler: swiftType = "MTLSamplerState"
-            case .texture: swiftType = "MTLTexture"
-            default: continue
+        var swiftNameLookup: [String: String] = [:]
+        var swiftTypeLookup: [String: String] = [:]
+        for declaration in self.customDeclarations {
+            if case .swiftParameterName(let oldName, let newName) = declaration {
+                swiftNameLookup[oldName] = newName
             }
-            builder.add(line: "public var \(parameter.name): \(swiftType)? = nil")
+            
+            if case .swiftParameterType(let parameter, let type) = declaration {
+                swiftTypeLookup[parameter] = type
+            }
+        }
+        
+        for parameter in self.parameters {
+            var swiftType = swiftTypeLookup[parameter.name]
+            
+            let hasLookup = swiftType != nil
             
             switch parameter.kind {
             case .buffer:
-                builder.add(line: "public var \(parameter.name)Offset: Int = 0")
+                if !hasLookup {
+                    swiftType = "MTLBuffer"
+                }
+            case .sampler:
+                if swiftType != nil {
+                    print("WARNING: Swift Types are not available for sampler parameters, ignoring \(swiftType!)")
+                }
+                swiftType = "MTLSamplerState"
+            case .texture:
+                if swiftType != nil {
+                    print("WARNING: Swift Types are not available for texture parameters, ignoring \(swiftType!)")
+                }
+                swiftType = "MTLTexture"
+            default:
+                if swiftType != nil {
+                    print("WARNING: Swift Types are only available for buffer parameters, ignoring \(swiftType!)")
+                }
+                continue
+            }
+            builder.add(line: "\(accessLevel.rawValue) var \(swiftNameLookup[parameter.name] ?? parameter.name): \(swiftType!)? = nil")
+            
+            switch parameter.kind {
+            case .buffer:
+                if !hasLookup {
+                    builder.add(line: "\(accessLevel.rawValue) var \(swiftNameLookup[parameter.name] ?? parameter.name)Offset: Int = 0")
+                }
             default: continue
             }
         }
         
         builder.add(line: "")
         
-        builder.add(line: "public init(library: MTLLibrary) throws {")
+        builder.add(line: "\(accessLevel.rawValue) init(library: MTLLibrary) throws {")
         builder.pushLevel()
         
         builder.add(line: "self.pipelineState = try library.computePipelineState(function: \"\(self.name)\")")
@@ -88,17 +145,30 @@ public struct MTLShader {
         
         builder.add(line: "")
         
-        builder.add(line: "func encode(using encoder: MTLComputeCommandEncoder) {")
+        builder.add(line: "\(accessLevel.rawValue) func encode(using encoder: MTLComputeCommandEncoder) {")
         builder.pushLevel()
         
         for parameter in self.parameters {
             switch parameter.kind {
             case .buffer:
-                builder.add(line: "encoder.setBuffer(self.\(parameter.name), offset: self.\(parameter.name)Offset, index: \(parameter.index!))")
+                if let _ = swiftTypeLookup[parameter.name] {
+                    builder.add(line: "if let _\(swiftNameLookup[parameter.name] ?? parameter.name) = self.\(swiftNameLookup[parameter.name] ?? parameter.name) {")
+                    builder.pushLevel()
+                    builder.add(line: "encoder.set(_\(swiftNameLookup[parameter.name] ?? parameter.name), at: \(parameter.index!))")
+                    builder.popLevel()
+                    builder.add(line: "} else {")
+                    builder.pushLevel()
+                    builder.add(line: "encoder.setBuffer(nil, offset: 0, index: \(parameter.index!))")
+                    builder.popLevel()
+                    builder.add(line: "}")
+                } else {
+                    builder.add(line: "encoder.setBuffer(self.\(swiftNameLookup[parameter.name] ?? parameter.name), offset: self.\(parameter.name)Offset, index: \(parameter.index!))")
+                }
+                
             case .sampler:
-                builder.add(line: "encoder.setSamplerState(self.\(parameter.name), index: \(parameter.index!))")
+                builder.add(line: "encoder.setSamplerState(self.\(swiftNameLookup[parameter.name] ?? parameter.name), index: \(parameter.index!))")
             case .texture:
-                builder.add(line: "encoder.setTexture(self.\(parameter.name), index: \(parameter.index!))")
+                builder.add(line: "encoder.setTexture(self.\(swiftNameLookup[parameter.name] ?? parameter.name), index: \(parameter.index!))")
             default: continue
             }
         }

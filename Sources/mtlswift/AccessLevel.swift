da876446b1f515860d505acd84bfc52b34279653
s1ddok <siddok@gmail.com>
2019-02-07 20:49:34 +0300

    introduce global AccessLevel enum

---
+++ Sources/mtlswift/AccessLevel.swift
@@ -0,0 +1,14 @@
+//
+//  AccessLevel.swift
+//  mtlswift
+//
+//  Created by Andrey Volodin on 07/02/2019.
+//
+
+public enum AccessLevel: String {
+    case `public` = "public"
+    case `private` = "private"
+    case `fileprivate` = "fileprivate"
+    case `internal` = "internal"
+    case `open` = "open"
+}

b11ae7b521c9e5bd520b8c0572b5896799ad52e2
s1ddok <siddok@gmail.com>
2019-09-25 13:27:31 +0300

    threadgroup memory length

--- Sources/mtlswift/Models/ThreadgroupSize.swift
+++ Sources/mtlswift/Models/ThreadgroupSize.swift
@@ -11,3 +11,10 @@ public enum ThreadgroupSize {
     case constant(x: Int, y: Int, z: Int)
     case provided
 }
+
+public enum ThreadgroupMemoryLength {
+    case providedTotal
+    case providedPerThread
+    case total(bytes: Int)
+    case thread(bytes: Int)
+}

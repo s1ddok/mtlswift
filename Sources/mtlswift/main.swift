3467056da4129699fca52040c504c4b6ff92a6fe
s1ddok <siddok@gmail.com>
2019-09-26 15:45:21 +0300

    support watching, currently leaks memory

--- Sources/mtlswift/main.swift
+++ Sources/mtlswift/main.swift
@@ -1,33 +1,61 @@
 import Foundation
 
 do {
-    let start = Date()
-    let path = CommandLine.arguments[1]
-
-    guard let url = URL(string: path) else {
-        throw "Passed string is not a valid URL"
-    }
-
+    let firstArgument = CommandLine.arguments[1]
+    let secondArgument = CommandLine.arguments[2]
     let shaderGenerator = ShaderGenerator()
 
-    if url.pathExtension == "metal" {
-        try shaderGenerator.generateShaders(for: URL(fileURLWithPath: url.absoluteString))
-    } else {
-        guard let enumerator = FileManager.default.enumerator(atPath: path) else {
-            throw "Couldn't access niether file nor directory at \(path)"
-        }
+    var observers: [FileObserver] = []
 
-        while let metalFile = enumerator.nextObject() as? NSString {
-            if metalFile.hasSuffix("metal") {
-                let metalFileURL = URL(fileURLWithPath: url.path + "/" + (metalFile as String))
-                try shaderGenerator.generateShaders(for: metalFileURL)
+    if firstArgument == "watch" {
+        guard let url = URL(string: secondArgument) else {
+            throw ""
+        }
+        if url.pathExtension == "metal" {
+            let fileURL = URL(fileURLWithPath: url.absoluteString)
+
+            let observer = FileObserver(file: fileURL)
+            observer.start {
+                try? shaderGenerator.generateShaders(for: fileURL)
+            }
+            observers.append(observer)
+        } else {
+            guard let enumerator = FileManager.default.enumerator(atPath: secondArgument) else {
+                throw "Couldn't access neither file nor directory at \(firstArgument)"
+            }
+
+            while let metalFile = enumerator.nextObject() as? NSString {
+                if metalFile.hasSuffix("metal") {
+                    let metalFileURL = URL(fileURLWithPath: url.path + "/" + (metalFile as String))
+                    let observer = FileObserver(file: metalFileURL)
+                    observer.start {
+                        try? shaderGenerator.generateShaders(for: metalFileURL)
+                    }
+                    observers.append(observer)
+                }
             }
         }
+
+        RunLoop.main.run()
+    } else if let url = URL(string: firstArgument) {
+        if url.pathExtension == "metal" {
+            let fileURL = URL(fileURLWithPath: url.absoluteString)
+            try shaderGenerator.generateShaders(for: fileURL)
+        } else {
+            guard let enumerator = FileManager.default.enumerator(atPath: firstArgument) else {
+                throw "Couldn't access neither file nor directory at \(firstArgument)"
+            }
+
+            while let metalFile = enumerator.nextObject() as? NSString {
+                if metalFile.hasSuffix("metal") {
+                    let metalFileURL = URL(fileURLWithPath: url.path + "/" + (metalFile as String))
+                    try shaderGenerator.generateShaders(for: metalFileURL)
+                }
+            }
+        }
+    } else {
+        throw "Passed string is not a valid URL"
     }
-
-    print("Finished in \(Date().timeIntervalSince(start))")
-
-    RunLoop.main.run()
 } catch {
     print(error.localizedDescription)
 }

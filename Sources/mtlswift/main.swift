ead9921198b1b88139357411491000de4ae54f31
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-10-25 17:06:38 +0300

    remove predefined args

--- Sources/mtlswift/main.swift
+++ Sources/mtlswift/main.swift
@@ -7,9 +7,4 @@ let encoderGenerator = EncoderGenerator()
 mtlSwift.commands = [WatchCommand(encoderGenerator: encoderGenerator),
                      GenerateCommand(encoderGenerator: encoderGenerator)]
 
-_ = mtlSwift.go(with: ["watch",
-                       "/Users/eugenebokhan/Desktop/Shaders1.metal",
-                       "/Users/eugenebokhan/Desktop/Shaders2.metal",
-                       "-o",
-                       "/Users/eugenebokhan/Desktop/Encoders.swift",
-                       ])
+_ = mtlSwift.go()
